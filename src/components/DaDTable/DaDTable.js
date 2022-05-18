7e43968d9318371851b54656b0662d5ca43f2038
SinkLine_P <popovartem505@gmail.com>
2022-05-19 01:17:31 +0300

    create drag-and-drop

--- src/components/DaDTable/DaDTable.js
+++ src/components/DaDTable/DaDTable.js
@@ -25,15 +25,85 @@ const DaDTable = (props) => {
     });
   }
 
+  const [currentBoard, setCurrentBoard] = useState(null);
+  const [currentItem, setCurrentItem] = useState(null);
+
+  function dragOverHandler(e) {
+    e.preventDefault();
+    if (e.target.className === "item") {
+      e.target.style.boxShadow = "0 4px 3px gray";
+    }
+  }
+  function dragLeaveHandler(e) {
+    e.target.style.boxShadow = "none";
+  }
+  function dragStartHandler(e, board, item) {
+    setCurrentBoard(board);
+    setCurrentItem(item);
+  }
+
+  function dragEndHandler(e) {
+    e.target.style.boxShadow = "none";
+  }
+  function dropHandler(e, board, item) {
+    e.preventDefault();
+    const currentIndex = currentBoard.items.indexOf(currentItem);
+    currentBoard.items.splice(currentIndex, 1);
+    const dropIndex = board.items.indexOf(item);
+    board.items.splice(dropIndex + 1, 0, currentItem);
+    setBoards(
+      boards.map((b) => {
+        if (b.id === board.id) {
+          return board;
+        }
+        if (b.id === currentBoard.id) {
+          return currentBoard;
+        }
+        return b;
+      })
+    );
+  }
+
+  function dropCardHandler(e, board) {
+    board.items.push(currentItem);
+    const currentIndex = currentBoard.items.indexOf(currentItem);
+    currentBoard.items.splice(currentIndex, 1);
+    setBoards(
+      boards.map((b) => {
+        if (b.id === board.id) {
+          return board;
+        }
+        if (b.id === currentBoard.id) {
+          return currentBoard;
+        }
+        return b;
+      })
+    );
+  }
+
   return (
     <div className={"app"}>
       {boards.map((board, key) => (
-        <div key={key} className={"board"}>
+        <div
+          key={key}
+          className={"board"}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDrop={(e) => dropCardHandler(e, board)}
+        >
           <div className={"board__title"}>{board.title}</div>
           {board.items.map((item, key) => (
-            <div key={key} className={"item"}>
-              <b>{item.title}</b>
-              <p>{item.desc}</p>
+            <div
+              onDragOver={(e) => dragOverHandler(e)}
+              onDragLeave={(e) => dragLeaveHandler(e)}
+              onDragStart={(e) => dragStartHandler(e, board, item)}
+              onDragEnd={(e) => dragEndHandler(e)}
+              onDrop={(e) => dropHandler(e, board, item)}
+              key={key}
+              className={"item"}
+              draggable={true}
+            >
+              {item.title}
+              {/*<p>{item.desc}</p>*/}
             </div>
           ))}
         </div>
