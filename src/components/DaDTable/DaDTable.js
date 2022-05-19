9e67bb80cba5862d02756b6c18c8f516b902eea2
SinkLine_P <popovartem505@gmail.com>
2022-05-19 12:08:58 +0300

    fix "one column blocked drag-and-drop"

--- src/components/DaDTable/DaDTable.js
+++ src/components/DaDTable/DaDTable.js
@@ -8,20 +8,22 @@ import "./Styles/index.scss";
 const DaDTable = (props) => {
   const { data } = props;
   const [boards, setBoards] = useState([
-    { id: 1, title: "Past Launches", items: [] },
-    { id: 2, title: "Launches", items: [] },
-    { id: 3, title: "My Launches", items: [] },
+    { id: 1, type: "board", title: "Past Launches", items: [] },
+    { id: 2, type: "board", title: "Launches", items: [] },
+    { id: 3, type: "board", title: "My Launches", items: [] },
   ]);
-  const twentyFlights = data.slice(0, 10);
+  const countFlights = data.slice(0, 10);
   if (
     boards[0].items.length === 0 &&
     boards[1].items.length === 0 &&
     boards[2].items.length === 0
   ) {
     let id = 1;
-    twentyFlights.map((e) => {
+    countFlights.map((e) => {
       boards[1].items.push({
         id: id,
+        type: "launch",
+        extendBoard: 0,
         title: e.name,
         desc: "Falcon " + id,
       });
@@ -31,89 +33,127 @@ const DaDTable = (props) => {
 
   const [currentBoard, setCurrentBoard] = useState(null);
   const [currentItem, setCurrentItem] = useState(null);
+  const [drugg, isDraggable] = useState(true);
 
   function dragOverHandler(e) {
     e.preventDefault();
+    // console.log("drag Over Handler");
     if (e.target.className === "item") {
       e.target.style.boxShadow = "0 4px 3px gray";
     }
   }
 
   function dragLeaveHandler(e) {
+    console.log("drag leave handler");
     e.target.style.boxShadow = "none";
   }
 
   function dragStartHandler(e, board, item) {
-    setCurrentBoard(board);
-    setCurrentItem(item);
+    console.log("drag start handler");
+    console.log(e);
+    console.log(board);
+    console.log(item);
+    console.log("===============");
+
+    if (board.id !== 1) {
+      setCurrentBoard(board);
+      setCurrentItem(item);
+    }
   }
 
   function dragEndHandler(e) {
+    console.log("drag end handler");
     e.target.style.boxShadow = "none";
   }
 
   function dropHandler(e, board, item) {
+    console.log("drop handler");
     e.preventDefault();
     e.stopPropagation();
-    const currentIndex = currentBoard.items.indexOf(currentItem);
-    currentBoard.items.splice(currentIndex, 1);
-    const dropIndex = board.items.indexOf(item);
-    board.items.splice(dropIndex + 1, 0, currentItem);
-    setBoards(
-      boards.map((b) => {
-        if (b.id === board.id) {
-          return board;
-        }
-        if (b.id === currentBoard.id) {
-          return currentBoard;
-        }
-        return b;
-      })
-    );
+    if (board.id !== 1) {
+      const currentIndex = currentBoard.items.indexOf(currentItem);
+      currentBoard.items.splice(currentIndex, 1);
+      const dropIndex = board.items.indexOf(item);
+      board.items.splice(dropIndex + 1, 0, currentItem);
+      setBoards(
+        boards.map((b) => {
+          if (b.id === board.id) {
+            return board;
+          }
+          if (b.id === currentBoard.id) {
+            return currentBoard;
+          }
+          return b;
+        })
+      );
+    }
   }
 
   function dropCardHandler(e, board) {
-    board.items.push(currentItem);
-    const currentIndex = currentBoard.items.indexOf(currentItem);
-    currentBoard.items.splice(currentIndex, 1);
-    setBoards(
-      boards.map((b) => {
-        if (b.id === board.id) {
-          return board;
-        }
-        if (b.id === currentBoard.id) {
-          return currentBoard;
-        }
-        return b;
-      })
-    );
+    console.log("drop card handler");
+    if (board.id !== 1) {
+      board.items.push(currentItem);
+      const currentIndex = currentBoard.items.indexOf(currentItem);
+      currentBoard.items.splice(currentIndex, 1);
+      setBoards(
+        boards.map((b) => {
+          if (b.id === board.id) {
+            return board;
+          }
+          if (b.id === currentBoard.id) {
+            return currentBoard;
+          }
+          return b;
+        })
+      );
+    }
+  }
+
+  function draggable(e) {
+    const targetExtendBoard = e.target.id;
+    if (targetExtendBoard === "1") {
+      console.log("drugg: false");
+      isDraggable(false);
+    } else {
+      console.log("drugg: true");
+      isDraggable(true);
+    }
   }
 
   return (
     <div className={"app"}>
       {boards.map((board, key) => (
         <div
+          id={board.id}
           key={key}
           className={"board"}
           onDragOver={(e) => dragOverHandler(e)}
           onDrop={(e) => dropCardHandler(e, board)}
+          onMouseOver={(e) => draggable(e)}
         >
-          <div className={"board__title"}>{board.title}</div>
-          {board.items.map((item, key) => (
-            <div
-              onDragOver={(e) => dragOverHandler(e)}
-              onDragLeave={(e) => dragLeaveHandler(e)}
-              onDragStart={(e) => dragStartHandler(e, board, item)}
-              onDragEnd={(e) => dragEndHandler(e)}
-              onDrop={(e) => dropHandler(e, board, item)}
-              key={key}
-              className={"item"}
-              draggable={true}
-            >
-              <b>{item.title}</b>
-              <p>{item.desc}</p>
-            </div>
-          ))}
+          <div id={board.id} className={"board__title"}>
+            {board.title}
+          </div>
+          {board.items.map((item, key) => {
+            item.extendBoard = board.id;
+
+            return (
+              <div
+                id={item.extendBoard}
+                onDragOver={(e) => dragOverHandler(e)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragStart={(e) => dragStartHandler(e, board, item)}
+                onDragEnd={(e) => dragEndHandler(e)}
+                onDrop={(e) => dropHandler(e, board, item)}
+                key={key}
+                className={"item"}
+                draggable={drugg}
+              >
+                <b id={item.extendBoard}>{item.title}</b>
+                <p id={item.extendBoard}>{item.desc}</p>
+              </div>
+            );
+          })}
         </div>
       ))}
     </div>
