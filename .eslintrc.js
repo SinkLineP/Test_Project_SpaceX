6d15ec88caa018cf74e2185934a08894cc122ed0
SinkLine_P <popovartem505@gmail.com>
2022-05-19 22:29:42 +0300

    add "ESLint configuration" and "logo image"

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es2021": true,
+  },
+  "globals": {
+    "window": true,
+    "self": true,
+    "document": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-unused-vars": "off"
+  }
+}
