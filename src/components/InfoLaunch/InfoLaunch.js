19a993865aff886efd3bb710feb6da75a3854754
SinkLine_P <popovartem505@gmail.com>
2022-05-22 17:51:47 +0300

    fix "go to launch page and go to home page, and no crash"

--- src/components/InfoLaunch/InfoLaunch.js
+++ src/components/InfoLaunch/InfoLaunch.js
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import "./Styles/index.scss";
 //redux
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const InfoLaunch = () => {
   const paramsURL = useParams();
+  let newArray;
+
+  useEffect(() => {
+    async function fetchData() {
+      let itemID = 1;
+      await axios.get("https://api.spacexdata.com/v4/launches").then((res) => {
+        newArray = res.data;
+
+        newArray.map((item) => {
+          item.itemID = itemID;
+          item.desc = "Falcon" + itemID;
+          item.title = item.name;
+          item.extendBoard = 0;
+          itemID++;
+        });
+
+        newArray.slice(1, 10);
+      });
+    }
+    fetchData().then((r) => r);
+  }, []);
 
   function isItems(launch) {
     return Number(launch.itemID) === Number(paramsURL.itemID);
   }
 
-  const oneLaunch = useSelector((state) => state.counter.data?.find(isItems));
+  const oneLaunch = useSelector((state) =>
+    state.counter.boardStore[0].items?.find(isItems)
+  );
+
+  const twoLaunch = useSelector((state) =>
+    state.counter.boardStore[1].items?.find(isItems)
+  );
+
+  const threeLaunch = useSelector((state) =>
+    state.counter.boardStore[2].items?.find(isItems)
+  );
 
   if (!oneLaunch) {
     return <h2>Launch not found!</h2>;
   }
+
+  console.log(oneLaunch);
+  console.log(twoLaunch);
+  console.log(threeLaunch);
 
   return (
     <>
@@ -27,13 +63,20 @@ const InfoLaunch = () => {
           </Link>
         </div>
         <p>
-          Title: <span>{oneLaunch.title}</span>
+          Title:{" "}
+          <span>{oneLaunch.title || twoLaunch.title || threeLaunch.title}</span>
         </p>
         <p>
-          Description: <span>{oneLaunch.desc}</span>
+          Description:{" "}
+          <span>{oneLaunch.desc || twoLaunch.desc || threeLaunch.desc}</span>
         </p>
         <p>
-          Extend Board: <span>{oneLaunch.extendBoard}</span>
+          Extend Board:{" "}
+          <span>
+            {oneLaunch.extendBoard ||
+              twoLaunch.extendBoard ||
+              threeLaunch.extendBoard}
+          </span>
         </p>
       </div>
     </>
